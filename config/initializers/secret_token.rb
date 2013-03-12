8cf4571407b9a7e635fb269d8d60857c2353159e
Eric Lindvall <eric@5stops.com>
2013-03-12 16:05:59 -0700

    Totally off the rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pilfer::Application.config.secret_token = 'a233f7b65530c2ba38932c7c7047971b8687a53023465ac82709a96bdbca1a6b6487d12dbee2588e2a5374c6584be2a83bf68f02cd70b0a025c14fb737f0474b'
