dc31a6f40b74e28b4ed7c96caf4d612e6476ae1d
Eric Lindvall <eric@5stops.com>
2013-03-12 19:04:58 -0700

    Clarify how to be secure

--- README.md
+++ README.md
@@ -14,3 +14,7 @@ and one of:
     GITHUB_AUTH_ORG=<organization_name>
     GITHUB_AUTH_TEAM=<team_id>
 
+Later on:
+
+    rake secret # to generate a secret token
+    heroku config:set SECRET_TOKEN={{your secret token}}
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,12 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Pilfer::Application.config.secret_token = 'a233f7b65530c2ba38932c7c7047971b8687a53023465ac82709a96bdbca1a6b6487d12dbee2588e2a5374c6584be2a83bf68f02cd70b0a025c14fb737f0474b'
+
+if Rails.env.production? && ENV['SECRET_TOKEN'].blank?
+  raise 'SECRET_TOKEN environment variable must be set!'
+end
+
+
+Pilfer::Application.config.secret_token =
+  ENV['SECRET_TOKEN'] ||
+  'a233f7b65530c2ba38932c7c7047971b8687a53023465ac82709a96bdbca1a6b6487d12dbee2588e2a5374c6584be2a83bf68f02cd70b0a025c14fb737f0474b'
