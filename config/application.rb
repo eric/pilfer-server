ee7fbe9cfe1e52b01a049b03b7c56a3620141f59
Larry Marburger <larry@marburger.cc>
2013-03-15 11:17:03 -0400

    Prevent loading app when precompiling assets

--- config/application.rb
+++ config/application.rb
@@ -60,6 +60,10 @@ module Pilfer
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # Prevent loading app when precompiling assets.
+    # https://devcenter.heroku.com/articles/rails-asset-pipeline
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
