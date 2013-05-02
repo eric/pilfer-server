7e2ac214c0a11b223a4a2bf7cd063d1fc62f04dc
Larry Marburger <larry@marburger.cc>
2013-05-02 15:34:51 -0400

    Remove generated code

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,19 +3,14 @@ class ProfilesController < ApplicationController
 
   helper_method :profile_value_for_sort
 
-  # GET /profiles
-  # GET /profiles.json
   def index
     @profiles = @app.profiles
 
     respond_to do |format|
-      format.html # index.html.erb
-      # format.json { render json: @profiles }
+      format.html
     end
   end
 
-  # GET /profiles/1
-  # GET /profiles/1.json
   def show
     @profile = @app.profiles.find(params[:id])
 
@@ -27,68 +22,16 @@ class ProfilesController < ApplicationController
     @sorted_profile = sorted_profile(@profile, @sort, @summary)
 
     respond_to do |format|
-      format.html # show.html.erb
-      # format.json { render json: @profile }
+      format.html
     end
   end
 
-  # # GET /profiles/new
-  # # GET /profiles/new.json
-  # def new
-  #   @profile = @app.profiles.build
-  #
-  #   respond_to do |format|
-  #     format.html # new.html.erb
-  #     # format.json { render json: @profile }
-  #   end
-  # end
-  #
-  # # GET /profiles/1/edit
-  # def edit
-  #   @profile = @app.profiles.find(params[:id])
-  # end
-  #
-  # # POST /profiles
-  # # POST /profiles.json
-  # def create
-  #   @profile = @app.profiles.build(params[:profile])
-  #
-  #   respond_to do |format|
-  #     if @profile.save
-  #       format.html { redirect_to @profile, notice: 'Profile was successfully created.' }
-  #       # format.json { render json: @profile, status: :created, location: @profile }
-  #     else
-  #       format.html { render action: "new" }
-  #       # format.json { render json: @profile.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-  #
-  # # PUT /profiles/1
-  # # PUT /profiles/1.json
-  # def update
-  #   @profile = @app.profiles.find(params[:id])
-  #
-  #   respond_to do |format|
-  #     if @profile.update_attributes(params[:profile])
-  #       format.html { redirect_to @profile, notice: 'Profile was successfully updated.' }
-  #       # format.json { head :no_content }
-  #     else
-  #       format.html { render action: "edit" }
-  #       # format.json { render json: @profile.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  # DELETE /profiles/1
-  # DELETE /profiles/1.json
   def destroy
     @profile = @app.profiles.find(params[:id])
     @profile.destroy
 
     respond_to do |format|
       format.html { redirect_to profiles_url }
-      # format.json { head :no_content }
     end
   end
 
@@ -96,7 +39,6 @@ class ProfilesController < ApplicationController
   def fetch_app
     @app = App.find(params[:app_id])
   end
-
 
   def profile_value_for_sort(file_profile, sort, summary)
     if summary == 'exclusive'
