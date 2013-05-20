db29f112b20c9fb7cf2befdb5d41cbe669d75786
Dan Thompson <dwthompson@gmail.com>
2013-05-19 23:29:43 -0400

    Remove generated controller code

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,87 +1,66 @@
 class AppsController < ApplicationController
-  # GET /apps
-  # GET /apps.json
+  before_action :fetch_app, except: [:index, :new, :create]
+
   def index
     @apps = App.all
 
     respond_to do |format|
-      format.html # index.html.erb
-      # format.json { render json: @apps }
+      format.html 
     end
   end
 
-  # GET /apps/1
-  # GET /apps/1.json
   def show
-    @app = App.find(params[:id])
-
     respond_to do |format|
-      format.html # show.html.erb
-      # format.json { render json: @app }
+      format.html 
     end
   end
 
-  # GET /apps/new
-  # GET /apps/new.json
   def new
     @app = App.new
 
     respond_to do |format|
-      format.html # new.html.erb
-      # format.json { render json: @app }
+      format.html 
     end
   end
 
-  # GET /apps/1/edit
   def edit
-    @app = App.find(params[:id])
   end
 
-  # POST /apps
-  # POST /apps.json
   def create
     @app = App.new(app_params)
 
     respond_to do |format|
       if @app.save
         format.html { redirect_to apps_path, notice: 'App was successfully created.' }
-        # format.json { render json: @app, status: :created, location: @app }
       else
         format.html { render action: "new" }
-        # format.json { render json: @app.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PUT /apps/1
-  # PUT /apps/1.json
   def update
-    @app = App.find(params[:id])
-
     respond_to do |format|
       if @app.update(app_params)
         format.html { redirect_to apps_path, notice: 'App was successfully updated.' }
-        # format.json { head :no_content }
       else
         format.html { render action: "edit" }
-        # format.json { render json: @app.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /apps/1
-  # DELETE /apps/1.json
   def destroy
-    @app = App.find(params[:id])
     @app.destroy
 
     respond_to do |format|
       format.html { redirect_to apps_path }
-      # format.json { head :no_content }
     end
   end
 
   private
+
+  def fetch_app
+    @app = App.find(params[:id])
+  end
 
   def app_params
     params.require(:app).permit(:name)
